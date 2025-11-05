ee04a843bc8c94045c84a9dd30df909982d0fed6
akashsoftsuave <akashsoftsuave@gmail.com>
2025-11-05 14:28:11 +0530

    middleware added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,19 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeOrm.config';
 import { NotesModule } from './notes/notes.module';
+import { LoggerMiddleware } from './middleware/logger.middleware';
 
 @Module({
   imports: [TypeOrmModule.forRoot(typeOrmConfig), UsersModule, NotesModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
